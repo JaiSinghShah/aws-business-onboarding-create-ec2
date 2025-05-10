fcde91f626c2423e390f492de10533bfd53a19b4
JaiSinghShah <113639120+JaiSinghShah@users.noreply.github.com>
2025-05-10 19:45:45 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,34 +2,12 @@ provider "aws" {
   region = "ap-south-1"
 }
 
-# Fetch latest Ubuntu 20.04 LTS AMI
-data "aws_ami" "ubuntu" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-}
-
-# Security Group allowing SSH and HTTP access
 resource "aws_security_group" "web_sg" {
-  name        = "web-sg"
-  description = "Allow SSH and HTTP"
+  name        = "Web-SG"
+  description = "Allow HTTP traffic"
 
   ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
+    description = "Allow HTTP inbound"
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
@@ -37,6 +15,7 @@ resource "aws_security_group" "web_sg" {
   }
 
   egress {
+    description = "Allow all outbound"
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
@@ -44,21 +23,20 @@ resource "aws_security_group" "web_sg" {
   }
 }
 
-# EC2 Instance
 resource "aws_instance" "web" {
-  ami           = data.aws_ami.ubuntu.id
-  instance_type = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.web_sg.id]
+  ami                         = "ami-002f6e91abff6eb96"  # Amazon Linux 2 (ap-south-1)
+  instance_type               = "t2.micro"
+  vpc_security_group_ids      = [aws_security_group.web_sg.id]
+
+  user_data = <<EOF
+#!/bin/bash
+yum update -y
+yum install -y httpd
+systemctl start httpd
+systemctl enable httpd
+EOF
 
   tags = {
-    Name = "Development"
+    Name = "Web-Server"
   }
-
-  user_data = <<-EOF
-              #!/bin/bash
-              apt update -y
-              apt install -y apache2
-              systemctl enable apache2
-              systemctl start apache2
-              EOF
 }

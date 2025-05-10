a40f5720b3487f370c7f077c7bc37ed4dea010e2
JaiSinghShah <113639120+JaiSinghShah@users.noreply.github.com>
2025-05-10 17:58:09 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -29,7 +29,7 @@ resource "aws_security_group" "my_ec2_sg" {
 }
 
 resource "aws_instance" "my_ec2" {
-  ami           = "ami-0e35ddab05955cf57"  # Replace with your AMI ID
+  ami           = "ami-06b6e5225d1db5f46"  # Replace with your AMI ID
   instance_type = var.instance_type
   security_groups = [aws_security_group.my_ec2_sg.name]
   

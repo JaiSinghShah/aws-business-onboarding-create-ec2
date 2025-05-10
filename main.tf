eea29bc5d911a449e7902580951cf034cca13cdc
JaiSinghShah <113639120+JaiSinghShah@users.noreply.github.com>
2025-05-10 19:53:43 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,33 +15,10 @@ data "aws_subnets" "default" {
   }
 }
 
-resource "aws_security_group" "web_sg" {
-  name        = "Web-SG"
-  description = "Allow HTTP traffic"
-  vpc_id      = data.aws_vpc.default.id
-
-  ingress {
-    description = "Allow HTTP inbound"
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    description = "Allow all outbound"
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
 resource "aws_instance" "web" {
-  ami                         = "ami-002f6e91abff6eb96"
-  instance_type               = "t2.micro"
-  subnet_id                   = data.aws_subnets.default.ids[0]
-  vpc_security_group_ids      = [aws_security_group.web_sg.id]
+  ami           = "ami-002f6e91abff6eb96"
+  instance_type = "t2.micro"
+  subnet_id     = data.aws_subnets.default.ids[0]
 
   user_data = <<EOF
 #!/bin/bash

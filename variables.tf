e79075bb3b78a7913ab9c6fe6fbf895707d75339
JaiSinghShah <113639120+JaiSinghShah@users.noreply.github.com>
2025-05-10 17:51:54 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,1 +1,12 @@
+variable "aws_region" {
+  description = "The AWS region where the resources will be deployed"
+  type        = string
+  default     = "us-east-1" # You can change it to the region of your choice
+}
+
+variable "instance_type" {
+  description = "Type of EC2 instance to launch"
+  type        = string
+  default     = "t2.micro" # You can change it to a different instance type
+}
 
